2869e936511ab55aa065e824f10991800717ebbe
Zhenya Shishkin <040796@gmail.com>
2020-02-29 18:48:57 +0300

    add basic models

---
+++ data_storage/models.py
@@ -0,0 +1,55 @@
+from django.db import models
+
+
+# Create your models here.
+
+
+class NeuralModel(models.Model):
+    id = models.CharField(primary_key=True, max_length=30)
+    user_id = models.CharField(max_length=30)
+    execution_code_url = models.URLField()
+
+
+class UserInput(models.Model):
+    id = models.CharField(primary_key=True, max_length=30)
+    user_id = models.CharField(max_length=30)
+    data_url = models.URLField()
+
+
+class TrainingTask(models.Model):
+    WAITING = 'WAITING'
+    RUNNING = 'RUNNING'
+    REJECTED = 'REJECTED'
+    FAILED = 'FAILED'
+    SUCCEEDED = 'SUCCEEDED'
+
+    STATUS_CHOICES = [
+        (WAITING, 'Waiting'),
+        (RUNNING, 'Running'),
+        (REJECTED, 'Rejected'),
+        (FAILED, 'Failed'),
+        (SUCCEEDED, 'Succeeded'),
+    ]
+
+    id = models.CharField(primary_key=True, max_length=30)
+    user_id = models.CharField(max_length=30, editable=False)
+
+    model = models.ForeignKey(
+        NeuralModel,
+        models.PROTECT,
+        editable=False,
+    )
+    user_input = models.ForeignKey(
+        UserInput,
+        models.PROTECT,
+        editable=False,
+    )
+    parameters = models.CharField(
+        help_text='Parameters in json',
+        max_length=4096,
+        editable=False,
+    )
+
+    status = models.CharField(max_length=30, choices=STATUS_CHOICES)
+    error_message = models.CharField(max_length=1024, blank=True)
+    result_url = models.URLField(blank=True)
