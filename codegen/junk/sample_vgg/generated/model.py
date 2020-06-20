1f8019832774476e3ed2c929b12f510d3dc55802
Roman Nikonov <niconneo@yandex.ru>
2020-06-20 18:05:58 +0300

    codegen: add vgg example

---
+++ codegen/junk/sample_vgg/generated/model.py
@@ -0,0 +1,60 @@
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Conv2D, Dense, Flatten, MaxPool2D
+
+
+def init_model():
+    model = Sequential()
+    model.add(
+        Conv2D(
+            input_shape=[28, 28, 1],
+            filters=64,
+            kernel_size=[3, 3],
+            padding="same",
+            activation="relu",
+        )
+    )
+    model.add(
+        Conv2D(filters=64, kernel_size=[3, 3], padding="same", activation="relu",)
+    )
+    model.add(MaxPool2D(pool_size=[2, 2], strides=[2, 2],))
+    model.add(
+        Conv2D(filters=128, kernel_size=[3, 3], padding="same", activation="relu",)
+    )
+    model.add(
+        Conv2D(filters=128, kernel_size=[3, 3], padding="same", activation="relu",)
+    )
+    model.add(MaxPool2D(pool_size=[2, 2], strides=[2, 2],))
+    model.add(
+        Conv2D(filters=256, kernel_size=[3, 3], padding="same", activation="relu",)
+    )
+    model.add(
+        Conv2D(filters=256, kernel_size=[3, 3], padding="same", activation="relu",)
+    )
+    model.add(
+        Conv2D(filters=256, kernel_size=[3, 3], padding="same", activation="relu",)
+    )
+    model.add(MaxPool2D(pool_size=[2, 2], strides=[2, 2],))
+    model.add(
+        Conv2D(filters=512, kernel_size=[3, 3], padding="same", activation="relu",)
+    )
+    model.add(
+        Conv2D(filters=512, kernel_size=[3, 3], padding="same", activation="relu",)
+    )
+    model.add(
+        Conv2D(filters=512, kernel_size=[3, 3], padding="same", activation="relu",)
+    )
+    model.add(MaxPool2D(pool_size=[2, 2], strides=[2, 2],))
+    model.add(
+        Conv2D(filters=512, kernel_size=[3, 3], padding="same", activation="relu",)
+    )
+    model.add(
+        Conv2D(filters=512, kernel_size=[3, 3], padding="same", activation="relu",)
+    )
+    model.add(
+        Conv2D(filters=512, kernel_size=[3, 3], padding="same", activation="relu",)
+    )
+    model.add(Flatten())
+    model.add(Dense(units=4096, activation="relu",))
+    model.add(Dense(units=4096, activation="relu",))
+    model.add(Dense(units=1, activation="softmax",))
+    return model
---
+++ codegen/junk/sample_vgg/generated/ng_input.py
@@ -0,0 +1,30 @@
+import ng_config
+import util
+
+
+class NeurogenIO:
+    def __init__(self, ng_bus):
+        self.ng_bus = ng_bus
+
+    # Returns list of CLI vars
+    def get_vars(self):
+        return ["train_test_ratio"]
+
+    def read_train_data(self):
+        from tensorflow.keras.datasets import mnist
+
+        data = mnist.load_data()
+        (self.X_train, self.y_train), (self.X_test, self.y_test) = data
+        return data
+
+    def get_train_xy(self):
+        self.X_train = self.X_train.reshape(self.X_train.shape[0], 28, 28, 1)
+        self.X_train = self.X_train.astype("float32")
+        self.X_train /= 255
+        return self.X_train[:-100], self.y_train[:-100]
+
+    def get_test_xy(self):
+        self.X_test = self.X_test.reshape(self.X_test.shape[0], 28, 28, 1)
+        self.X_test = self.X_test.astype("float32")
+        self.X_test /= 255
+        return self.X_test[-100:], self.y_test[-100:]
